61161bd8ebd7989c8e08fe42e63037fd0304d107
Christiangonzal <39070144+Christiangonzal@users.noreply.github.com>
2018-05-07 21:57:55 -0400

    Update about.php

--- about.php
+++ about.php
@@ -8,42 +8,59 @@
     <title>Your Trip | Welcome</title>
 	<link rel="stylesheet" href="./css/header_style.css">
 	<link rel="stylesheet" href="./css/footer_style.css">
+	<link rel="stylesheet" href="./css/about_style.css">
 </head>
 
 <body>
 <!--include the header--> 
     <p><?php include ('./include/header.html'); ?></p>
-<div >
-<p style="font-size:160%;" style="font-family:verdana;">Who are we?</p>
-<p>
-We are five computer science students working on our associates degree.<br/>
-We wish to enter a four year school in order to get our Bachelor of Science.<br/>
-</p>
-<p style="font-size:160%;" style="font-family:verdana;">Creators and founders of LIFE TRAVEL:</p>
-<p>
-Anastasiya Ayala<br/>
-Patrick Diston<br/>
-Christian Gonzalez<br/>
-Masanao Yukawa<br/>
-Yuanhe Ye<br/>
-</p>
-<p style="font-size:160%;" style="font-family:verdana;">Why us?</p>
-<p>
-Planning a holiday can be both a difficult and expensive task.  
-By offering a means to plan a holiday is nothing new, we believe that by applying a social-network-like element and allowing users to meet other holiday-goes on their travels, we can offer something new and unique whilst also demonstrating an understanding of database technology.
-Holidays can prove to be extremely expensive, and most people only get a few days each year.  Therefore, it is important to ensure that we are doing all we can to maximize the way we are using that time and money
-LIFETRAVEL seeks to help holiday-goers make the best possible use of their time.
-We strive to make your experience easy as possible and provide you with the fast information for you to plan your trip around the world accordingly. 
-</p>
-
-</p>
-<p style="font-size:160%;" style="font-family:verdana;">Advantages?<p>
-<p>	
-Can highlight other attractions in an area to broaden users’ choice of activities
-Can offers users a group experience to add something special to their holiday
-The tag-based user review system would allow algorithms to accurately suggest relevant locations to users based on their own preferences or trip history
-
-</p>
+<div class="a">
+  <div class ="title">
+   <p>Who are we?</b></p>
+  </div>
+  <div class="body">
+   <p>We are five computer science students working on our associates degree.<br/>
+      We wish to enter a four year school in order to get our Bachelor of Science.<br/></p> 
+  </div>
+  <div class="title">
+	<p>Creators and founders of LIFE TRAVEL:<br/></p>
+  </div>
+  <div class="body2">
+	
+	<p>Anastasiya Ayala</p>
+	<p>Patrick Diston</p>
+	<p>Christian Gonzalez</p>
+	<p>Masanao Yukawa</p>
+	<p>Yuanhe Ye</p>
+    
+  </div>
+  <div class="title">
+    <p>Why us?</p>
+  </div>
+  <div class="body">
+    <p>Planning a holiday can be both a difficult and expensive task.<br/>
+	   By offering a means to plan a holiday is nothing new, we believe 
+	   that by applying a social-network-like element <br/> and allowing users 
+	   to meet other holiday-goes on their travels, we can offer something 
+	   new and unique whilst also demonstrating an understanding of database 
+	   technology.<br/>Holidays can prove to be extremely expensive, and most people 
+	   only get a few days each year.<br/>  Therefore, it is important to ensure that 
+	   we are doing all we can to maximize the way we are using that time and money
+	   LIFETRAVEL seeks to help holiday-goers make the best possible use of their time.<br/>
+	   We strive to make your experience easy as possible and provide you with the fast 
+	   information for you to plan your trip around the world accordingly.<br/>
+    </p>
+  </div>
+  <div class="tittle">
+    <p>Advantages?<p>
+  </div>
+  <div class="body">
+    <p>	Can highlight other attractions in an area to broaden users’ choice of activities
+	    Can offers users a group experience to add something special to their holiday.<br/>
+		The tag-based user review system would allow algorithms to accurately suggest relevant 
+		locations to users based on their own preferences or trip history.<br/>
+    </p>
+  </div>
 </div>	
 <!--include the footer--> 
 	<p><?php include ('./include/footer.html'); ?> </p>
