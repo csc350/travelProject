9fa769ea9aba947efd37f3511976052b1658663c
Christiangonzal <39070144+Christiangonzal@users.noreply.github.com>
2018-05-14 23:31:07 -0400

    Update Japan.php

--- Japan.php
+++ Japan.php
@@ -6,10 +6,23 @@
 	<meta name="keywords" content="travel, trip plan">
   	<meta name="author" content="CSC 350 Team">
     <title>Your Trip | Welcome</title>
-	<link rel="stylesheet" href="Japan_stylesheet.css" >
+	<link rel="stylesheet" href="./css/Japan_stylesheet.css" >
 	<link rel="stylesheet" href="./css/header_style.css">
 	<link rel="stylesheet" href="./css/footer_style.css">
   </head>
+  
+  <!--include the header--> 
+	 <p><?php 
+	    // echo $_SESSION['user_id']; 
+	     if (!isset($_SESSION['user_id'])){ 
+		 
+		     include ('./include/header.html');
+		 }
+		 else
+		 {
+			 include ('./include/header_user.html'); 
+		 }
+	?></p>
 
 
  <body>
@@ -17,23 +30,26 @@
 <div class="raw1">
     <div class="place city1">
 	   <img src = "http://www.destinasian.com/travel/wp-content/uploads/tokyo-sky-tower1.jpg" alt ="Tokyo" width = "300" height ="225">
+	   <br/>
 	   <a href="https://www.japan-guide.com/e/e2164.html">Tokyo</a>
 	</div>
 	
 	
 	<div class="place city2">
 	   <img src="https://phgcdn.com/images/uploads/NRTYR/corporatemasthead/yokohama-royal-park-hotel.jpg" alt ="Yokohama"width ="300" height="225"> 
+	   <br/>
 	   <a href="https://www.japan-guide.com/e/e2156.html">Yokohama</a>
 	</div>
 </div>  
 <div class="raw1">
 	<div class="places cities1">
 	   <img src ="https://languages.utah.edu/study-abroad/Osaka%20Japan%202.jpg"alt ="Osaka" width ="300" height="225" > 
-	   
+	   <br/>
 	   <a href="https://www.japan-guide.com/e/e2157.html">Osaka</a>
 	</div>
 	<div class="places cities2">
 	   <img src="http://bdtech24.com/wp-content/uploads/2016/03/Osu-Kannon-Temple-The-Best-Attraction-Places-in-Nagoya-Japan.jpg" alt ="Nagoya"width = "300" height = "225"> 
+	   <br/>
 	   <a href="https://www.japan-guide.com/e/e2155.html">Nagoya</a>
 	</div>
 	
@@ -41,6 +57,7 @@
 	
 	<div class="places cities3">
 	   <img src="http://www.worldfortravel.com/wp-content/uploads/2013/02/Temple-in-Japanese-Kyoto-Gardens.jpg" alt ="Kyoto" width="300" height="225"> 
+	   <br/>
 	   <a href="https://www.japan-guide.com/e/e2158.html">Kyoto</a>	
 </div>
  </body>
